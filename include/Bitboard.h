6aad9579732e66a9984d15e3752d4026f6d25972
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-09-27 20:00:32 +0930

    Update Bitboard class name and piece comments

--- include/Bitboard.h
+++ include/Bitboard.h
@@ -1,24 +1,24 @@
 #include <cstdint>
 #include <stdexcept>
 
-class BitBoard
+class Bitboard
 {
 private:
 	uint64_t bb;
 
 public:
-	BitBoard(uint64_t bitboard) : bb(bitboard) {}
+	Bitboard(uint64_t bitboard) : bb(bitboard) {}
 	/** Returns a copy shifted north */
-	BitBoard north() const { return bb << 8; }
+	Bitboard north() const { return bb << 8; }
 	/** Returns a copy shifted south */
-	BitBoard south() const { return bb >> 8; }
+	Bitboard south() const { return bb >> 8; }
 	/** Returns a copy shifted east */
-	BitBoard east(int times = 1) const { return bb << times; }
+	Bitboard east(int times = 1) const { return bb << times; }
 	/** Returns a copy shifted west */
-	BitBoard west(int times = 1) const { return bb >> times; }
+	Bitboard west(int times = 1) const { return bb >> times; }
 	/** Returns a BitBoard which only contains all bits in common between this and the other */
-	BitBoard inter(const BitBoard &bb) const { return bb.bb & this->bb; }
+	Bitboard inter(const Bitboard &bb) const { return bb.bb & this->bb; }
 	/** Returns a BitBoard which contains all bits in either this and the other */
-	BitBoard unions(const BitBoard &bb) const { return bb.bb | this->bb; }
+	Bitboard unions(const Bitboard &bb) const { return bb.bb | this->bb; }
 	bool has_one_bit() const { throw std::logic_error("Not yet implemented. "); }
 };
--- include/Piece.hpp
+++ include/Piece.hpp
@@ -1,5 +1,5 @@
 #include <vector>
-#include "BitBoard.h"
+#include "Bitboard.h"
 #include "Action.h"
 
 namespace Chess
@@ -14,21 +14,21 @@ namespace Chess
 		};
 
 	protected:
-		BitBoard m_location;
+		Bitboard m_location;
 		Color m_color;
 
 	public:
-		Piece(BitBoard location, Color color) : m_location(location), m_color(color) {}
+		Piece(Bitboard location, Color color) : m_location(location), m_color(color) {}
 		/** Get valid moves of the target piece. 
-	 * @param allies A bitboard which contains the location of all allies. 
-	 * @param enemies A bitboard which contains the location of all enemies. 
-	 * @param history The history of all moves made this game. 
-	 * @returns Valid locations in which this piece can move
-	*/
-		virtual BitBoard get_moves(BitBoard white, BitBoard black, const std::vector<Action> &history) const = 0;
+		 * @param allies A bitboard which contains the location of all allies. 
+		 * @param enemies A bitboard which contains the location of all enemies. 
+		 * @param history The history of all moves made this game. 
+		 * @returns Valid locations in which this piece can move
+		 */
+		virtual Bitboard get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const = 0;
 		/** Get the piece's character. 
-	 * @return The character that represents this piece in chess notation. 
-	 */
+		 * @return The character that represents this piece in chess notation. 
+		 */
 		virtual char get_piece_character() const = 0;
 	};
 } // namespace Chess
