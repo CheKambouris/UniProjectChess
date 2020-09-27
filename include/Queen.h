a8430f0226184aee7ff7eb389f825f0f5cc66e16
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-09-27 20:43:52 +0930

    Add header files for all pieces

---
+++ include/Queen.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Piece.hpp"
+
+namespace Chess
+{
+	class Queen: Piece
+	{
+	public:
+		Queen(Bitboard location, Color color);
+		Bitboard get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const override;
+		char get_piece_character() const override;
+	};
+}
