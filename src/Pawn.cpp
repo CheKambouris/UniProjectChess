ae93341f287bc0b897475b71ba1b118c0c7cd720
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-01 18:45:47 +0930

    Implement pawn movement. Missing En Passant.

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -2,14 +2,20 @@
 
 using namespace Chess;
 
-Pawn::Pawn(Bitboard location, Color color) : Piece(location, color) {}
+Pawn::Pawn(Bitboard location, Color color) : Piece(location, color) { }
 
-Bitboard Pawn::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const
-{
-    return Bitboard(0);
+Bitboard Pawn::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const {
+	Bitboard legal_moves;
+	auto front = get_color() == Color::White? &Bitboard::north: &Bitboard::south;
+	legal_moves += (get_location().*front)().inter(!allies);
+	legal_moves += (get_location().*front)().west().inter(enemies);
+	legal_moves += (get_location().*front)().east().inter(enemies);
+	// Todo: En passant
+
+	return legal_moves;
 }
 
 char Pawn::get_piece_character() const
 {
-    return '\0';
+	return '\0';
 }
