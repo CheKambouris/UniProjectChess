084b78db3da656d2e3cb707ee732c9b3dd2eebe7
Javad Hamidi <19621911+javadhamidi@users.noreply.github.com>
2020-10-14 10:17:45 +1030

    Created basic game interface in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,35 @@ using namespace Chess;
 int main(int argc, char const *argv[])
 {
 	Board board = Board();
-	Bitboard filled_spaces;
+	// Bitboard filled_spaces;
 
-	for (auto &&p : board.get_pieces())
+	// for (auto &&p : board.get_pieces())
+	// {
+	// 	filled_spaces += p->get_location();
+	// }
+	// std::cout << filled_spaces.to_string() << std::endl;
+
+
+	std::string input;
+
+	std::cout << board.to_string() << std::endl;
+
+	while (1)
 	{
-		filled_spaces += p->get_location();
+		std::cout << ">>> ";
+
+		std::getline(std::cin, input);
+
+		if (input == "quit") { break; };
+
+		if (board.move(input.substr(0, input.find(" ")), input.substr(input.find(" ") + 1, input.length())))
+		{
+			std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+			std::cout << board.to_string() << std::endl;
+		}
+		else { std::cout << "Invalid move, try again!" << std::endl; }
 	}
-	std::cout << filled_spaces.to_string() << std::endl;
-
-	std::cout << "\n\n";
-
-	std::cout << board.to_string() << std::endl;
-
-	board.move("b2", "c4");
-
-	std::cout << "\n\n";
-
-	std::cout << board.to_string() << std::endl;
+	std::cout << "\n Bye!" << std::endl;
 
 	return 0;
 }
