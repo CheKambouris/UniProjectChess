81ed6d27a1fdd7c45dd5834bd571545d94bb0025
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-15 15:29:33 +1030

    Put in logic for King::move

--- src/King.cpp
+++ src/King.cpp
@@ -4,12 +4,19 @@ using namespace Chess;
 
 King::King(Bitboard location, Color color) : Piece(location, color) {}
 
-Bitboard King::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const
-{
-    return Bitboard(0);
+Bitboard King::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action>& history) const {
+	Bitboard moves;
+	moves += this->get_location().north().inter(!allies);
+	moves += this->get_location().south().inter(!allies);
+	moves += this->get_location().east().inter(!allies);
+	moves += this->get_location().west().inter(!allies);
+	moves += this->get_location().north().east().inter(!allies);
+	moves += this->get_location().north().west().inter(!allies);
+	moves += this->get_location().south().east().inter(!allies);
+	moves += this->get_location().south().west().inter(!allies);
+	return moves;
 }
 
-char King::get_piece_character() const
-{
-    return 'K';
+char King::get_piece_character() const {
+	return 'K';
 }
