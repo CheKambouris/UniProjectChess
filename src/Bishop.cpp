2d016fce1a703242e3ec4c01523c102cbd9547c8
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-15 16:07:25 +1030

    Add logic to Bishop::move

--- src/Bishop.cpp
+++ src/Bishop.cpp
@@ -4,12 +4,27 @@ using namespace Chess;
 
 Bishop::Bishop(Bitboard location, Color color) : Piece(location, color) {}
 
-Bitboard Bishop::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const
-{
-    return Bitboard(0);
+Bitboard Bishop::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action>& history) const {
+	Bitboard legal_moves;
+	int moves = 0;
+	while(this->get_location().north(moves).west(moves).inter(!allies)) {
+		legal_moves += this->get_location().north(moves).west(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().north(moves).east(moves).inter(!allies)) {
+		legal_moves += this->get_location().north(moves).east(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().south(moves).east(moves).inter(!allies)) {
+		legal_moves += this->get_location().south(moves).east(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().south(moves).west(moves).inter(!allies)) {
+		legal_moves += this->get_location().south(moves).west(moves).inter(!allies);
+	}
+	return legal_moves;
 }
 
-char Bishop::get_piece_character() const
-{
-    return 'B';
+char Bishop::get_piece_character() const {
+	return 'B';
 }
