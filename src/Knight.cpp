39f240bf0466c3abaf7237499810d3760acd6068
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-15 15:54:56 +1030

    Add logic to Knight::moves

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -4,12 +4,19 @@ using namespace Chess;
 
 Knight::Knight(Bitboard location, Color color) : Piece(location, color) {}
 
-Bitboard Knight::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const
-{
-    return Bitboard(0);
+Bitboard Knight::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action>& history) const {
+	Bitboard legal_moves;
+	legal_moves += this->get_location().north(2).east() .inter(!allies);
+	legal_moves += this->get_location().north(2).west() .inter(!allies);
+	legal_moves += this->get_location().west(2) .north().inter(!allies);
+	legal_moves += this->get_location().west(2) .south().inter(!allies);
+	legal_moves += this->get_location().south(2).east() .inter(!allies);
+	legal_moves += this->get_location().south(2).west() .inter(!allies);
+	legal_moves += this->get_location().east(2) .north().inter(!allies);
+	legal_moves += this->get_location().east(2) .south().inter(!allies);
+	return legal_moves;
 }
 
-char Knight::get_piece_character() const
-{
-    return 'N';
+char Knight::get_piece_character() const {
+	return 'N';
 }
