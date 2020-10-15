2bcf6af83cf8e6d985ef9cc456af780688688a68
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-15 16:19:38 +1030

    Add logic for Queen::move

--- src/Queen.cpp
+++ src/Queen.cpp
@@ -4,12 +4,42 @@ using namespace Chess;
 
 Queen::Queen(Bitboard location, Color color) : Piece(location, color) {}
 
-Bitboard Queen::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const
-{
-    return Bitboard(0);
+Bitboard Queen::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action>& history) const {
+	Bitboard legal_moves;
+	int moves = 0;
+	while (this->get_location().north(moves).inter(!allies)) {
+		legal_moves += this->get_location().north(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().east(moves).inter(!allies)) {
+		legal_moves += this->get_location().east(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().south(moves).inter(!allies)) {
+		legal_moves += this->get_location().south(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().west(moves).inter(!allies)) {
+		legal_moves += this->get_location().west(moves).inter(!allies);
+	}
+	while (this->get_location().north(moves).west(moves).inter(!allies)) {
+		legal_moves += this->get_location().north(moves).west(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().north(moves).east(moves).inter(!allies)) {
+		legal_moves += this->get_location().north(moves).east(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().south(moves).east(moves).inter(!allies)) {
+		legal_moves += this->get_location().south(moves).east(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().south(moves).west(moves).inter(!allies)) {
+		legal_moves += this->get_location().south(moves).west(moves).inter(!allies);
+	}
+	return legal_moves;
 }
 
-char Queen::get_piece_character() const
-{
-    return 'Q';
+char Queen::get_piece_character() const {
+	return 'Q';
 }
