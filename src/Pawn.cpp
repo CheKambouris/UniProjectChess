deed133a920a5f6939f188618137c493b2dfa985
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-15 15:37:21 +1030

    Add logic for double push

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -10,8 +10,13 @@ Bitboard Pawn::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Ac
 	legal_moves += (get_location().*front)().inter(!allies);
 	legal_moves += (get_location().*front)().west().inter(enemies);
 	legal_moves += (get_location().*front)().east().inter(enemies);
+	if ((this->get_color() == Piece::White && this->get_location().get_row() == 1) ||
+		(this->get_color() == Piece::Black && this->get_location().get_row() == 6)) {
+		if ((get_location().*front)().inter(!allies) != 0) {
+			legal_moves += ((get_location().*front)().*front)().inter(!allies); // Double push
+		}
+	}
 	// Todo: En passant
-
 	return legal_moves;
 }
 
