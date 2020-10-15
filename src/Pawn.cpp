81e8d41c4cbf93389d4bd0af2c41aa4d21926749
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-15 16:29:43 +1030

    Fix error caused by changing Bitboard::north and south propertied

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -7,13 +7,13 @@ Pawn::Pawn(Bitboard location, Color color) : Piece(location, color) { }
 Bitboard Pawn::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const {
 	Bitboard legal_moves;
 	auto front = get_color() == Color::White? &Bitboard::north: &Bitboard::south;
-	legal_moves += (get_location().*front)().inter(!allies);
-	legal_moves += (get_location().*front)().west().inter(enemies);
-	legal_moves += (get_location().*front)().east().inter(enemies);
+	legal_moves += (get_location().*front)(1).inter(!allies);
+	legal_moves += (get_location().*front)(1).west().inter(enemies);
+	legal_moves += (get_location().*front)(1).east().inter(enemies);
 	if ((this->get_color() == Piece::White && this->get_location().get_row() == 1) ||
 		(this->get_color() == Piece::Black && this->get_location().get_row() == 6)) {
-		if ((get_location().*front)().inter(!allies) != 0) {
-			legal_moves += ((get_location().*front)().*front)().inter(!allies); // Double push
+		if ((get_location().*front)(1).inter(!allies)) {
+			legal_moves += (get_location().*front)(2).inter(!allies); // Double push
 		}
 	}
 	// Todo: En passant
