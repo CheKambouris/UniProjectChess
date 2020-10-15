a9f22a6fa9e5e8abf7b768a0505d756d37fb3ff1
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-15 16:45:54 +1030

    Fix and implement sliding pieces

--- src/Bishop.cpp
+++ src/Bishop.cpp
@@ -6,21 +6,25 @@ Bishop::Bishop(Bitboard location, Color color) : Piece(location, color) {}
 
 Bitboard Bishop::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action>& history) const {
 	Bitboard legal_moves;
-	int moves = 0;
+	int moves = 1;
 	while(this->get_location().north(moves).west(moves).inter(!allies)) {
 		legal_moves += this->get_location().north(moves).west(moves).inter(!allies);
+		++moves;
 	}
-	moves = 0;
+	moves = 1;
 	while (this->get_location().north(moves).east(moves).inter(!allies)) {
 		legal_moves += this->get_location().north(moves).east(moves).inter(!allies);
+		++moves;
 	}
-	moves = 0;
+	moves = 1;
 	while (this->get_location().south(moves).east(moves).inter(!allies)) {
 		legal_moves += this->get_location().south(moves).east(moves).inter(!allies);
+		++moves;
 	}
-	moves = 0;
+	moves = 1;
 	while (this->get_location().south(moves).west(moves).inter(!allies)) {
 		legal_moves += this->get_location().south(moves).west(moves).inter(!allies);
+		++moves;
 	}
 	return legal_moves;
 }
--- src/Queen.cpp
+++ src/Queen.cpp
@@ -6,36 +6,44 @@ Queen::Queen(Bitboard location, Color color) : Piece(location, color) {}
 
 Bitboard Queen::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action>& history) const {
 	Bitboard legal_moves;
-	int moves = 0;
+	int moves = 1;
 	while (this->get_location().north(moves).inter(!allies)) {
 		legal_moves += this->get_location().north(moves).inter(!allies);
+		++moves;
 	}
-	moves = 0;
+	moves = 1;
 	while (this->get_location().east(moves).inter(!allies)) {
 		legal_moves += this->get_location().east(moves).inter(!allies);
+		++moves;
 	}
-	moves = 0;
+	moves = 1;
 	while (this->get_location().south(moves).inter(!allies)) {
 		legal_moves += this->get_location().south(moves).inter(!allies);
+		++moves;
 	}
-	moves = 0;
+	moves = 1;
 	while (this->get_location().west(moves).inter(!allies)) {
 		legal_moves += this->get_location().west(moves).inter(!allies);
+		++moves;
 	}
 	while (this->get_location().north(moves).west(moves).inter(!allies)) {
 		legal_moves += this->get_location().north(moves).west(moves).inter(!allies);
+		++moves;
 	}
-	moves = 0;
+	moves = 1;
 	while (this->get_location().north(moves).east(moves).inter(!allies)) {
 		legal_moves += this->get_location().north(moves).east(moves).inter(!allies);
+		++moves;
 	}
-	moves = 0;
+	moves = 1;
 	while (this->get_location().south(moves).east(moves).inter(!allies)) {
 		legal_moves += this->get_location().south(moves).east(moves).inter(!allies);
+		++moves;
 	}
-	moves = 0;
+	moves = 1;
 	while (this->get_location().south(moves).west(moves).inter(!allies)) {
 		legal_moves += this->get_location().south(moves).west(moves).inter(!allies);
+		++moves;
 	}
 	return legal_moves;
 }
