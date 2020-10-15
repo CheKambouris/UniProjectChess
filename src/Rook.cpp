171ae94a3511b77a10c9ba797033e1539116a0d2
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-15 16:19:16 +1030

    Add logic for Rook::move

--- src/Rook.cpp
+++ src/Rook.cpp
@@ -4,12 +4,27 @@ using namespace Chess;
 
 Rook::Rook(Bitboard location, Color color) : Piece(location, color) {}
 
-Bitboard Rook::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action> &history) const
-{
-    return Bitboard(0);
+Bitboard Rook::get_moves(Bitboard allies, Bitboard enemies, const std::vector<Action>& history) const {
+	Bitboard legal_moves;
+	int moves = 0;
+	while (this->get_location().north(moves).inter(!allies)) {
+		legal_moves += this->get_location().north(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().east(moves).inter(!allies)) {
+		legal_moves += this->get_location().east(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().south(moves).inter(!allies)) {
+		legal_moves += this->get_location().south(moves).inter(!allies);
+	}
+	moves = 0;
+	while (this->get_location().west(moves).inter(!allies)) {
+		legal_moves += this->get_location().west(moves).inter(!allies);
+	}
+	return legal_moves;
 }
 
-char Rook::get_piece_character() const
-{
-    return 'R';
+char Rook::get_piece_character() const {
+	return 'R';
 }
