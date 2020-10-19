50bd5db479f28df72bb883e35f43a437b67d3910
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-19 13:07:44 +1030

    Fix namspace error caused by last commit in BoardTest

--- tests/BoardTest.cpp
+++ tests/BoardTest.cpp
@@ -2,11 +2,11 @@
 #include <gtest/gtest.h>
 
 TEST(BoardTest, GetBitboardFromNotation) {
-	Bitboard result1 = Board::get_bitboard_notation("a1");
+	Bitboard result1 = Chess::Board::get_bitboard_notation("a1");
 	Bitboard expect1 = Bitboard(1);
-	Bitboard result2 = Board::get_bitboard_notation("e1");
+	Bitboard result2 = Chess::Board::get_bitboard_notation("e1");
 	Bitboard expect2 = Bitboard(1).east(4);
-	Bitboard result3 = Board::get_bitboard_notation("e2");
+	Bitboard result3 = Chess::Board::get_bitboard_notation("e2");
 	Bitboard expect3 = expect2.north();
 	EXPECT_EQ(result1, expect1);
 	EXPECT_EQ(result2, expect2);
