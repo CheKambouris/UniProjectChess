5442aa716de82135432a39bfd9b50bb0c6dae666
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-19 12:48:49 +1030

    Fix oversight where Board was not within the Chess namespace

--- include/Board.h
+++ include/Board.h
@@ -5,22 +5,22 @@
 #include <memory>
 #include <string>
 
-using namespace Chess;
-
-class Board
-{
-private:
-	std::vector<std::unique_ptr<Piece>> m_pieces;
-	Piece::Color m_current_turn;
-public:
-	Board();
-	const std::vector<std::unique_ptr<Piece>>& get_pieces();
-	Piece::Color get_current_turn();
-	void switch_current_turn();
-	std::string to_string();
-	std::wstring to_string_unicode();
-	bool move(std::string from, std::string to);
-	Bitboard get_team_moves(Piece::Color color);
-	Bitboard get_team_locations(Piece::Color color);
-	static Bitboard get_bitboard_notation(std::string str_location);
-};
+namespace Chess {
+	class Board
+	{
+	private:
+		std::vector<std::unique_ptr<Piece>> m_pieces;
+		Piece::Color m_current_turn;
+	public:
+		Board();
+		const std::vector<std::unique_ptr<Piece>>& get_pieces();
+		Piece::Color get_current_turn();
+		void switch_current_turn();
+		std::string to_string();
+		std::wstring to_string_unicode();
+		bool move(std::string from, std::string to);
+		Bitboard get_team_moves(Piece::Color color);
+		Bitboard get_team_locations(Piece::Color color);
+		static Bitboard get_bitboard_notation(std::string str_location);
+	};
+}
