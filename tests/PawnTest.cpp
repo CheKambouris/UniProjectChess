e015eac9d9d5768eb732258de9a2e731cf21d3e7
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-20 15:17:26 +1030

    Make pawn test slightly more readable

--- tests/PawnTest.cpp
+++ tests/PawnTest.cpp
@@ -2,21 +2,21 @@
 #include "Pieces.h"
 
 TEST(PawnTest, MoveMethod) {
-	Chess::Pawn w_pawn(Bitboard(1).north(), Chess::Piece::White); // at a2
-	Chess::Pawn b_pawn(Bitboard((uint64_t)1 << 8 * 6).south(), Chess::Piece::Black); // at a6
+	Chess::Pawn w_pawn(Bitboard(0, 1), Chess::Piece::White);
+	Chess::Pawn b_pawn(Bitboard(0, 5), Chess::Piece::Black); // at a6
 	Bitboard w_expect_moves1;
 	w_expect_moves1 += w_pawn.get_location().north();
-	w_expect_moves1 += w_pawn.get_location().north().north();
-	Bitboard w_expect_moves2;
-	w_expect_moves2 += Bitboard(w_pawn.get_location().north());
-	w_expect_moves2 += Bitboard(w_pawn.get_location().north().north());
-	w_expect_moves2 += w_pawn.get_location().north().east(); // Test attack
-	Bitboard b_expect_moves;
-	b_expect_moves += Bitboard(b_pawn.get_location().south());
+	w_expect_moves1 += w_pawn.get_location().north(2);
+	EXPECT_EQ(w_pawn.get_moves(0, 0, std::vector<Chess::Action>()), w_expect_moves1);
 
 	Bitboard black_piece_location = w_pawn.get_location().north().east();
 
-	EXPECT_EQ(w_pawn.get_moves(0, 0, std::vector<Chess::Action>()), w_expect_moves1);
+	Bitboard w_expect_moves2;
+	w_expect_moves2 += Bitboard(w_pawn.get_location().north());
+	w_expect_moves2 += Bitboard(w_pawn.get_location().north(2));
+	w_expect_moves2 += w_pawn.get_location().north().east(); // Test attack
 	EXPECT_EQ(w_pawn.get_moves(0, black_piece_location, std::vector<Chess::Action>()), w_expect_moves2);
+
+	Bitboard b_expect_moves = Bitboard(b_pawn.get_location().south());
 	EXPECT_EQ(b_pawn.get_moves(0, 0, std::vector<Chess::Action>()), b_expect_moves);
 }
