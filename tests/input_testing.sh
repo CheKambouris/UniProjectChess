310edc1b7325afcd70922ce9f39a60e8ae50a702
Javad Hamidi <19621911+javadhamidi@users.noreply.github.com>
2020-10-20 15:16:22 +1030

    Simplified input testing

--- tests/input_testing.sh
+++ tests/input_testing.sh
@@ -1,1 +1,14 @@
-cat tests/input_test_fools_mate.txt | build/debug/chess
+#!/bin/bash
+
+echo -e "\nTEST 1: FOOLS MATE"
+
+# cat tests/input_tests/input_test_fools_mate.txt | build/debug/chess
+test1=`cat tests/input_tests/input_test_fools_mate.txt | build/debug/chess`
+
+if [[ $test1 == *"Invalid move, try again!"* ]]; then
+    echo -e "WARNING - Invalid moves were made!"
+else
+   echo -e "PASSED"
+fi
+
+echo -e "\n"
