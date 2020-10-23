e7f16ddcda24fb62226f1ed5d1f07f360dbc0d32
Che Kambouris <a1800006@student.adelaide.edu.au>
2020-10-23 12:51:53 +1030

    Add helpful messages for the user and some commands

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,13 @@
 
 using namespace Chess;
 
+const std::string COMMANDS = 
+"quit: Exit the program\n"
+"ff: Surrender and exit, giving your enemy the satisfaction in knowing that they're better than you\n"
+"instructions: Shows the same instructions that are shown in the beginning of the game.\n"
+"help: Shows this message. ";
+
+
 const std::string INSTRUCTIONS = 
 "This is a chess game that utilises something based on Algebraic Chess Notation for input\n"
 "In order to move a piece, selected the piece by declaring its character, then its location "
@@ -18,7 +25,7 @@ const std::string INSTRUCTIONS =
 "Once you have constructed your move command, simply press enter. (Example command: Ra1 a7)\n"
 "When taking pieces, don't put the piece character that you're taking. "
 "When moving a pawn, they do not have a piece character. Just use the location. \n"
-"When checkmated, the you must give up as the game won't end it for you. \n"
+"When checkmated, you must give up as the game won't end for you. Type \"help\" for commands\n"
 "Piece Characters:\n"
 "King: K, Queen: Q, Bishop: B, Knight: K, Rook: R";
 
@@ -36,18 +43,36 @@ int main(int argc, char const *argv[])
 		std::string input;
 		std::getline(std::cin, input);
 
-		if (input == "quit") { break; };
+		if (input == "help") { 
+			std::wcout << L"----- COMMANDS -----" << std::endl;
+			std::wcout << converter.from_bytes(COMMANDS) << std::endl; 
+			continue;
+		};
+		if (input == "instructions") {
+			std::wcout << converter.from_bytes(INSTRUCTIONS) << std::endl;
+			continue;
+		};
+		if (input == "quit") {
+			break; 
+		};
+		if (input == "ff") {
+			auto winner = board.get_current_turn() == Piece::White ? L"Black": L"White";
+			std::wcout <<  winner << " wins, congrats " << winner << std::endl;
+			break;
+		}
+		if(input == "show") {
+			std::wcout << board.to_string_unicode() << std::endl;
+			continue;
+		}
 
 		if (board.move(input)) {
-			std::wcout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 			std::wcout << board.to_string_unicode() << std::endl;
 
 			board.switch_current_turn();
 		}
-		else { std::wcout << converter.from_bytes(board.get_move_error()) << std::endl;
+		else {
+			std::wcout << converter.from_bytes(board.get_move_error()) << std::endl;
 		}
 	}
-	std::wcout << "\n Bye!" << std::endl;
-
 	return 0;
 }
